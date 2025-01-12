de8b79f8216f0e6cccd006aa74b157e9c47fd93c
MaxThePrisberry <bodomo9999@gmail.com>
2025-01-11 22:41:35 -0700

    Finish delete service

--- server/src/main/java/service/DeleteService.java
+++ server/src/main/java/service/DeleteService.java
@@ -6,6 +6,8 @@ import dataaccess.UserDAO;
 
 public class DeleteService {
     public void clear(AuthDAO authDAO, GameDAO gameDAO, UserDAO userDAO) {
-        
+        authDAO.clear();
+        gameDAO.clear();
+        userDAO.clear();
     }
 }
