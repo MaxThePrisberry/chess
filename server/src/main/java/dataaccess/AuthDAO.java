d0ee124f22e793c633e4475dcc9edfa7817066b6
MaxThePrisberry <bodomo9999@gmail.com>
2025-01-11 21:00:17 -0700

    Implement AuthDAO.java

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,4 +1,32 @@
 package dataaccess;
 
+import model.AuthData;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class AuthDAO {
+
+    private Set<AuthData> database = new HashSet<>();
+
+    public void createAuth(String authToken, String username) {
+        database.add(new AuthData(authToken, username));
+    }
+
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        for (AuthData auth : database) {
+            if (auth.authToken().equals(authToken)) {
+                return auth;
+            }
+        }
+        throw new DataAccessException("getAuth Error: No AuthData with given authToken");
+    }
+
+    public void deleteAuth(String authToken) {
+        database.removeIf(auth -> auth.authToken().equals(authToken));
+    }
+
+    public void deleteAllAuth() {
+        database.clear();
+    }
 }
