4e67dae69ad8b5fbad56639c58d5c59351aa9009
MaxThePrisberry <bodomo9999@gmail.com>
2025-01-12 02:03:21 -0700

    Implement handler register functionality

--- server/src/main/java/server/ChessHandler.java
+++ server/src/main/java/server/ChessHandler.java
@@ -1,19 +1,26 @@
 package server;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import dataaccess.HandlerTargetedException;
 import service.Phase3MasterService;
+import service.model.RegisterRequest;
+import service.model.UserDataResult;
 import spark.Request;
 import spark.Response;
+
+import java.util.Map;
 
 public class ChessHandler {
     private final Gson gson = new Gson();
 
-    private Phase3MasterService service = new Phase3MasterService();
+    private final Phase3MasterService service = new Phase3MasterService();
 
     public String clear(Request req, Response res) {
         try {
             service.clear();
         } catch (Exception e) {
+            res.status(500);
             String message = "Error: " + e.getMessage();
             return gson.toJson(message);
         }
@@ -21,7 +28,28 @@ public class ChessHandler {
     }
 
     public String register(Request req, Response res) {
-
+        String message;
+        String body = req.body();
+        Map<String, String> request = gson.fromJson(body, new TypeToken<Map<String, String>>(){}.getType());
+        if (request.get("username") == null || request.get("username").isBlank() ||
+                request.get("password") == null || request.get("password").isBlank() ||
+                request.get("email") == null || request.get("email").isBlank()) {
+            res.status(400);
+            message = "Error: bad request";
+            return gson.toJson(message);
+        }
+        try {
+            UserDataResult user = service.register(new RegisterRequest(request.get("username"), request.get("password"), request.get("email")));
+            return gson.toJson(user);
+        } catch (HandlerTargetedException e) {
+            res.status(e.getErrorNumber());
+            message = e.getMessage();
+            return gson.toJson(message);
+        } catch (Exception e) {
+            res.status(500);
+            message = e.getMessage();
+            return gson.toJson(message);
+        }
     }
 
     public String login(Request req, Response res) {
