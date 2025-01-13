c7548667010e6c7fd86783caf3dc9e18385b7207
MaxThePrisberry <bodomo9999@gmail.com>
2025-01-12 17:10:50 -0700

    Implement database with UserDAO.java

--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -1,7 +1,12 @@
 package dataaccess;
 
+import model.AuthData;
 import model.UserData;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -9,24 +14,59 @@ public class UserDAO {
 
     private Set<UserData> database = new HashSet<>();
 
+    public UserDAO() {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            try (PreparedStatement statement = conn.prepareStatement("""
+                    CREATE TABLE IF NOT EXISTS users (
+                        email VARCHAR(255) NOT NULL,
+                        password VARCHAR(255) NOT NULL,
+                        username VARCHAR(50) NOT NULL UNIQUE
+            );""")) {
+                statement.executeUpdate();
+            }
+        } catch (SQLException | DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public void createUser(String username, String password, String email) {
-        database.add(new UserData(username, password, email));
+        try (Connection conn = DatabaseManager.getConnection()){
+            try (PreparedStatement createStatement = conn.prepareStatement("INSERT INTO users (username, password, email) " +
+                    "VALUES (?, ?, ?);")) {
+                createStatement.setString(1, username);
+                createStatement.setString(2, password);
+                createStatement.setString(3, email);
+                createStatement.executeUpdate();
+            }
+        } catch (DataAccessException | SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public UserData getUser(String username) throws DataAccessException {
-        for (UserData user : database) {
-            if (user.username().equals(username)) {
-                return user;
+        try (Connection conn = DatabaseManager.getConnection()){
+            try (PreparedStatement statement = conn.prepareStatement("SELECT username, password, email FROM users" +
+                    " WHERE username = ?;")) {
+                statement.setString(1, username);
+                ResultSet res = statement.executeQuery();
+                if (res.next()) {
+                    return new UserData(res.getString("username"), res.getString("password"),
+                            res.getString("email"));
+                }
             }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
         throw new DataAccessException("getUser Error: No user with given username");
     }
 
-    public void deleteUser(String username) {
-        database.removeIf(user -> user.username().equals(username));
-    }
-
     public void clear() {
-        database.clear();
+        try (Connection conn = DatabaseManager.getConnection()){
+            try (PreparedStatement statement = conn.prepareStatement("TRUNCATE TABLE users;")) {
+                statement.executeUpdate();
+            }
+        } catch (DataAccessException | SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
