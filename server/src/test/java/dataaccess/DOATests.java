50a3ac96764df2cbbd8a8a2ac537abfeee44e997
MaxThePrisberry <bodomo9999@gmail.com>
2025-01-12 19:34:47 -0700

    Write positive clear test

--- server/src/test/java/dataaccess/DOATests.java
+++ server/src/test/java/dataaccess/DOATests.java
@@ -1,10 +1,15 @@
 package dataaccess;
 
+import chess.ChessGame;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DOATests {
@@ -23,6 +28,10 @@ public class DOATests {
 
     @BeforeEach
     void setUp() {
+        UserDAO.setUp();
+        GameDAO.setUp();
+        AuthDAO.setUp();
+
         try (Connection conn = DatabaseManager.getConnection()){
             try (PreparedStatement statement = conn.prepareStatement("TRUNCATE TABLE auths;")) {
                 statement.executeUpdate();
@@ -38,5 +47,45 @@ public class DOATests {
         } catch (SQLException ignored) {}
     }
 
+    void assertEmpty() {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            try (PreparedStatement statement = conn.prepareStatement("SELECT COUNT(*) FROM users;")) {
+                ResultSet res = statement.executeQuery();
+                res.next();
+                assertEquals(0, res.getInt(1));
+            }
+            try (PreparedStatement statement = conn.prepareStatement("SELECT COUNT(*) FROM games;")) {
+                ResultSet res = statement.executeQuery();
+                res.next();
+                assertEquals(0, res.getInt(1));
+            }
+            try (PreparedStatement statement = conn.prepareStatement("SELECT COUNT(*) FROM auths;")) {
+                ResultSet res = statement.executeQuery();
+                res.next();
+                assertEquals(0, res.getInt(1));
+            }
+        } catch (DataAccessException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    @Test
+    @DisplayName("Clear Positive Test")
+    void clearValid() {
+        assertEmpty();
+
+        UserDAO.createUser("Potato", "Farmer", "a@us.f");
+        AuthDAO.createAuth("authtoken", "Potato");
+
+        ChessGame game = new ChessGame();
+        GameDAO.createGame("Potato", "Orange", "Fruit Bowl", game);
+
+        UserDAO.clear();
+        AuthDAO.clear();
+        GameDAO.clear();
+
+        assertEmpty();
+    }
+
+    
 }
