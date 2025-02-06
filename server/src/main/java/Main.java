bf719e87a770c928b3d6b84d0ec0818953221a0f
MaxThePrisberry <bodomo9999@gmail.com>
2025-02-06 13:43:09 -0700

    Change to randomized port

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -4,7 +4,7 @@ import server.Server;
 public class Main {
     public static void main(String[] args) {
         Server server = new Server();
-        server.run(8080);
+        server.run(0);
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
     }
